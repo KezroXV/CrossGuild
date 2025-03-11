b5712323a93bb76018ec885978796b24c913f52b
KezroXV <kezro10@gmail.com>
2025-03-11 11:55:13 +0100

    worked on 2nd page categories need to add brands to prisma and then push it to postgres

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Catehero from "@/components/Categories/CateHero";
 import CategoriesSection from "@/components/Categories/categoriesSection";
 import { TopSellingGamingGear } from "@/components/TopSellingGamingGear";
+import { Footer } from "@/components/ui/footer";
+import FooterSection from "@/components/footer";
 const page = () => {
   return (
     <div className="pt-4">
@@ -10,6 +12,7 @@ const page = () => {
       <Catehero />
       <CategoriesSection />
       <TopSellingGamingGear />
+      <FooterSection />
     </div>
   );
 };
