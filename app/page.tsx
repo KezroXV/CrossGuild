93b2ce4e34baec3e06e9eb9b1bc3f3219f930528
KezroXV <kezro10@gmail.com>
2025-10-12 22:26:32 +0100

    refactor: adjust padding in Home component for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   const session = await auth();
 
   return (
-    <div className="pt-px min-h-screen flex flex-col">
+    <div className="pt-2px min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1 flex flex-col gap-8">
         <HeroSection />
